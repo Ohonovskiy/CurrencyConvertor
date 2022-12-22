900c712b4cefb22146a665036a22f0f1455c47b6
Ohonovskiy <106039076+Ohonovskiy@users.noreply.github.com>
2022-12-22 03:51:10 +0200

    Edit

--- src/main/java/org/example/GetCurrency.java
+++ src/main/java/org/example/GetCurrency.java
@@ -3,9 +3,7 @@ package org.example;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-
 import java.io.IOException;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class GetCurrency {
