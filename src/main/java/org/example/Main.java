7e543ba32853abbee178ad2333078a61c18954af
ohonovskiy <yuriy.ohonovskiy@gmail.com>
2022-12-22 03:35:19 +0200

    Working parser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,10 @@
 package org.example;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Parser.Parse();
     }
 }
--- src/main/java/org/example/Parser.java
+++ src/main/java/org/example/Parser.java
@@ -4,17 +4,19 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Parser {
-    public static String Parse(){
+    public static String Parse() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello!\nEnter first currency");
         String first = scanner.nextLine();
         System.out.println("Enter second currency");
         String second = scanner.nextLine();
-        Document doc = Jsoup.parse("https://www.google.com/search?q=" + first + "+to+"+ second);
-        Element price = doc.getElementsByTag("DFlfde SwHCTb").first();
+        Document doc = Jsoup.connect("https://www.google.com/search?q=" + first + "+to+"+ second).get();
+        Element price = doc.selectFirst("span.DFlfde.SwHCTb");
+        System.out.println(price.text());
         return price.toString();
     }
 }
